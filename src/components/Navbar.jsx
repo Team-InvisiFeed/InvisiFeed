c92c969f3ec8b3dddc72eba243a8e61ec0f37edd
Sneha Sharma <snehav2109@gmail.com>
2025-03-19 23:58:09 +0530

    update navbar file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 "use client";
 import React from "react";
-import Link from "next/link";
 import { Button } from "./ui/button";
 
 function Navbar() {
@@ -10,33 +9,31 @@ function Navbar() {
     <nav className="p-4 md:p-6 bg-white text-gray-800 shadow-lg border-b-2">
       <div className="container mx-auto flex items-center justify-between">
         {/* Logo/Brand Name */}
-        <Link href="#" legacyBehavior>
-          <a className="text-2xl font-bold">InvisiFeed</a>
-        </Link>
+        <a href="#home" className="text-2xl font-bold">
+          InvisiFeed
+        </a>
 
         {/* Navigation Links */}
         <div className="hidden md:flex space-x-6">
-          <Link href="/" legacyBehavior>
-            <a className="text-gray-800 hover:text-gray-600">Home</a>
-          </Link>
-          <Link href="/about" legacyBehavior>
-            <a className="text-gray-800 hover:text-gray-600">AboutUs</a>
-          </Link>
-          <Link href="/businesses" legacyBehavior>
-            <a className="text-gray-800 hover:text-gray-600">Businesses</a>
-          </Link>
-          <Link href="/contact" legacyBehavior>
-            <a className="text-gray-800 hover:text-gray-600">ContactUs</a>
-          </Link>
+          <a href="#home" className="text-gray-800 hover:text-gray-600">
+            Home
+          </a>
+          <a href="#about" className="text-gray-800 hover:text-gray-600">
+            AboutUs
+          </a>
+          <a href="#businesses" className="text-gray-800 hover:text-gray-600">
+            Businesses
+          </a>
+          <a href="#contact" className="text-gray-800 hover:text-gray-600">
+            ContactUs
+          </a>
         </div>
 
         {/* Login Button */}
         {!session ? (
-          <Link href={"/sign-in"}>
-            <Button className="bg-gray-800 text-white hover:bg-gray-700">
-              Login
-            </Button>
-          </Link>
+          <Button className="bg-gray-800 text-white hover:bg-gray-700">
+            Login
+          </Button>
         ) : (
           <div className="flex items-center space-x-4">
             <span className="text-sm md:text-base">
