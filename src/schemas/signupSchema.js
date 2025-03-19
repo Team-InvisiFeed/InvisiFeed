f5c8e6f8cca2a49196fa6c084d14b08d51eff80b
Sneha Sharma <snehav2109@gmail.com>
2025-03-19 17:29:58 +0530

    update signup schema

--- src/schemas/signupSchema.js
+++ src/schemas/signupSchema.js
@@ -1,16 +1,6 @@
 import { z } from "zod";
 
-export const orgNameValidation = z
-  .string()
-  .min(2, "Organization Name must be at least 2 characters")
-  .max(20, "Organization Name must be no more than 20 characters")
-  .regex(
-    /^[A-Za-z0-9_ ]+$/,
-    "Organization Name must not contain special characters"
-  );
-
 export const signUpSchema = z.object({
-  organizationName: orgNameValidation,
   email: z.string().email({ message: "Invalid email address" }),
   password: z
     .string()
