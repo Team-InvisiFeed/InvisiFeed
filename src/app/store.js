bbc313510b6cd14ad59db5ccc26b9a865ca69478
Sneha Sharma <snehav2109@gmail.com>
2025-03-19 23:59:11 +0530

    add store.js

--- src/app/store.js
+++ src/app/store.js
@@ -8,22 +8,26 @@ const feedbackData = [
   {
     title: "Analysis of Ratings",
     mini: "Analyze ratings to identify trends and areas for improvement.",
-    image: "https://via.placeholder.com/150?text=Ratings+Analysis",
+    image:
+      "https://images.unsplash.com/photo-1633613286991-611fe299c4be?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     title: "User-Friendly Interface",
     mini: "A simple and intuitive platform for submitting feedback.",
-    image: "https://via.placeholder.com/150?text=User-Friendly",
+    image:
+      "https://images.unsplash.com/photo-1602064172250-43f8909056c7?q=80&w=2157&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     title: "Feedback Categorization",
     mini: "Automatically categorize feedback for better organization.",
-    image: "https://via.placeholder.com/150?text=Categorization",
+    image:
+      "https://plus.unsplash.com/premium_photo-1682310144714-cb77b1e6d64a?q=80&w=2112&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     title: "Insightful Reports",
     mini: "Generate detailed reports to understand user sentiments.",
-    image: "https://via.placeholder.com/150?text=Reports",
+    image:
+      "https://images.unsplash.com/photo-1460925895917-afdab827c52f?q=80&w=2015&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
 
