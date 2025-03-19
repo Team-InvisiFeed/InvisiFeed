def478c7a80ca88e883529cd23b8b142e9bea230
Sneha Sharma <snehav2109@gmail.com>
2025-03-19 20:03:19 +0530

    add store.js

---
+++ src/app/store.js
@@ -0,0 +1,30 @@
+const feedbackData = [
+  {
+    title: "Anonymity for Feedback Givers",
+    mini: "Users can submit feedback anonymously to ensure honest opinions.",
+    image:
+      "https://media.istockphoto.com/id/2155637046/photo/feedback-360-cycle-feedback.jpg?s=1024x1024&w=is&k=20&c=q9NtiWbR5TSuGMy7NLcC1i0WXVVtW1kQSwAkJV7qKbw=",
+  },
+  {
+    title: "Analysis of Ratings",
+    mini: "Analyze ratings to identify trends and areas for improvement.",
+    image: "https://via.placeholder.com/150?text=Ratings+Analysis",
+  },
+  {
+    title: "User-Friendly Interface",
+    mini: "A simple and intuitive platform for submitting feedback.",
+    image: "https://via.placeholder.com/150?text=User-Friendly",
+  },
+  {
+    title: "Feedback Categorization",
+    mini: "Automatically categorize feedback for better organization.",
+    image: "https://via.placeholder.com/150?text=Categorization",
+  },
+  {
+    title: "Insightful Reports",
+    mini: "Generate detailed reports to understand user sentiments.",
+    image: "https://via.placeholder.com/150?text=Reports",
+  },
+];
+
+export default feedbackData;
