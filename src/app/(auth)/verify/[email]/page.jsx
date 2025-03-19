493d972ee8a3a6a5bcaad37a2b2cea11ac161418
Shubh Verma <shubhverma2003@gmail.com>
2025-03-19 19:59:55 +0530

    update verify page

---
+++ src/app/(auth)/verify/[email]/page.jsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { toast } from "sonner"; // Replaced useToast with sonner
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { useParams, useRouter } from "next/navigation";
+import React from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+// Zod schema for validation
+const verifySchema = z.object({
+  code: z.string().min(6, "Code must be at least 6 characters"),
+});
+
+const VerifyAccount = () => {
+  const router = useRouter();
+  const params = useParams();
+
+  const form = useForm({
+    resolver: zodResolver(verifySchema),
+    defaultValues: {
+      code: "", // Ensure the input has a default value
+    },
+  });
+
+  const onSubmit = async (data) => {
+    try {
+      const response = await axios.post(`/api/verify-code`, {
+        email: params.email,
+        code: data.code,
+      });
+
+      toast.success(response.data.message);
+
+      router.replace("/sign-in");
+    } catch (error) {
+      console.error("Signup failed");
+      const errorMessage = error.response?.data?.message || "An error occurred";
+      toast.error(errorMessage);
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Verify Your Account
+          </h1>
+          <p className="mb-4">Enter the verification code sent to your email</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              name="code"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Verification Code</FormLabel>
+                  <Input placeholder="Code" {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Verify</Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default VerifyAccount;
