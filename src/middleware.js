98d6a2cb2d19afaf9b514bed51fbb30408ed63f9
Shubh Verma <shubhverma2003@gmail.com>
2025-03-20 16:26:11 +0530

    add auth middleware

---
+++ src/middleware.js
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from "next/server"; 
+import { getToken } from "next-auth/jwt"; 
+export { default } from "next-auth/middleware"; 
+
+export const config = {
+  matcher: [
+    "/user/:path*", 
+    "/sign-in", 
+    "/register", 
+    "/", 
+    "/verify/:path*", 
+  ],
+};
+
+export async function middleware(request) {
+  const token = await getToken({ req: request }); 
+  const url = request.nextUrl; 
+
+  console.log(token);
+  
+
+  if (
+    token && 
+    (url.pathname.startsWith("/sign-in") || 
+      url.pathname.startsWith("/register") || 
+      url.pathname.startsWith("/verify") || 
+      url.pathname === "/")
+  ) {
+    return NextResponse.redirect(new URL(`/user/${token.organizationName}`, request.url)); 
+  }
+
+  if (!token && url.pathname.startsWith("/user")) {
+    return NextResponse.redirect(new URL("/sign-in", request.url)); 
+  }
+
+  return NextResponse.next(); 
+}
