7b62fddafa6421303af425dc67397d4cdc2be220
Shubh Verma <shubhverma2003@gmail.com>
2025-03-22 00:23:57 +0530

    update submit-feedback api

--- src/app/api/submit-feedback/route.js
+++ src/app/api/submit-feedback/route.js
@@ -5,26 +5,46 @@ import { ApiError } from "@/utils/ApiError";
 export async function POST(req) {
   await dbConnect();
 
-  const { formData, username, invoiceNumber } = req.json();
+  const { formData, username, invoiceNumber } = await req.json();
 
-  const {
-    overallSatisfaction,
-    communication,
-    qualityOfService,
-    valueForMoney,
-    likelihoodToRecommend,
-    overallRating,
-    customFeedback,
-    suggestions,
-  } = formData;
+  console.log(formData);
+
+  const decodedUsername = decodeURIComponent(username);
+  // console.log(decodedUsername);
+
+  const decodedInvoiceNumber = decodeURIComponent(invoiceNumber);
+  // console.log(decodedInvoiceNumber);
 
   try {
-    const owner = OwnerModel.findOne({username});
+    const owner = await OwnerModel.findOne({ username: decodedUsername });
+    console.log("owner dekh : ", owner);
+
     if (!owner) {
-    throw new ApiError(404 , "Organisation not found");
+      throw new ApiError(404, "Organisation not found");
     }
+
+    owner.feedbacks.push(formData);
     
+    await owner.save();
+
+    const invoiceId = await owner.invoiceIds.some(
+      (id) => id === decodedInvoiceNumber
+    );    
+    console.log(invoiceId);
+    
+
+    if (invoiceId) {
+      await OwnerModel.updateOne(
+        { _id: owner._id },
+        { $pull: { invoiceIds: decodedInvoiceNumber } }
+      );
+    }
+
+    return Response.json(
+      { message: "Feedback added successfully" },
+      { status: 201 }
+    );
   } catch (error) {
-    
+    return Response.json({ message: error.message }, { status: 500 });
   }
 }
