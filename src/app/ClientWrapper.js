c492200335e68cecb26d5aff2b05bae46e7acd2f
Sneha Sharma <snehav2109@gmail.com>
2025-03-23 14:58:29 +0530

    add client wrapper

---
+++ src/app/ClientWrapper.js
@@ -0,0 +1,24 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+export default function ClientWrapper({ children }) {
+  const pathname = usePathname();
+
+  // Check if the route matches specific paths
+  const isFeedbackPage =
+    pathname.startsWith("/feedback") ||
+    pathname.startsWith("/sign-in") ||
+    pathname.startsWith("/register") ||
+    pathname.startsWith("/verify");
+
+  return (
+    <>
+      {!isFeedbackPage && <Navbar />}
+      <main>{children || <p>No children to render</p>}</main>
+      {!isFeedbackPage && <Footer />}
+    </>
+  );
+}
