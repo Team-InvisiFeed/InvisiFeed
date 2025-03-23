a375551c46c53038cb7999808bed8f7a02b7d6b4
Sneha Sharma <snehav2109@gmail.com>
2025-03-23 15:00:29 +0530

    add owner infor api

---
+++ src/app/api/get-owner-info/route.js
@@ -0,0 +1,27 @@
+import dbConnect from "@/lib/dbConnect";
+import OwnerModel from "@/model/Owner";
+import { ApiError } from "@/utils/ApiError";
+
+export async function POST(req) {
+  await dbConnect();
+
+  const { username } = await req.json();
+
+  const decodedUsername = decodeURIComponent(username);
+  console.log(decodedUsername);
+
+  try {
+    const owner = await OwnerModel.findOne({ username: decodedUsername });
+
+    if (!owner) {
+      throw new ApiError(404, "Organisation not found");
+    }
+
+    return Response.json(
+      { message: "Owner information retrieved successfully", data: owner },
+      { status: 201 }
+    );
+  } catch (error) {
+    return Response.json({ message: error.message }, { status: 500 });
+  }
+}
