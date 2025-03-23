b5dd5266dfa8368e8720f00c4010fa11b1800f9a
Sneha Sharma <snehav2109@gmail.com>
2025-03-23 11:27:28 +0530

    update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,34 +1,29 @@
-import { NextRequest, NextResponse } from "next/server"; 
-import { getToken } from "next-auth/jwt"; 
-export { default } from "next-auth/middleware"; 
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: [
-    "/user/:path*", 
-    "/sign-in", 
-    "/register", 
-    "/", 
-    "/verify/:path*", 
-  ],
+  matcher: ["/user/:path*", "/sign-in", "/register", "/", "/verify/:path*"],
 };
 
 export async function middleware(request) {
-  const token = await getToken({ req: request }); 
-  const url = request.nextUrl;   
+  const token = await getToken({ req: request });
+  const url = request.nextUrl;
 
   if (
-    token && 
-    (url.pathname.startsWith("/sign-in") || 
-      url.pathname.startsWith("/register") || 
-      url.pathname.startsWith("/verify") || 
-      url.pathname === "/")
+    token &&
+    (url.pathname.startsWith("/sign-in") ||
+      url.pathname.startsWith("/register") ||
+      url.pathname.startsWith("/verify"))
   ) {
-    return NextResponse.redirect(new URL(`/user/${token.username}`, request.url)); 
+    return NextResponse.redirect(
+      new URL(`/user/${token.username}`, request.url)
+    );
   }
 
   if (!token && url.pathname.startsWith("/user")) {
-    return NextResponse.redirect(new URL("/sign-in", request.url)); 
+    return NextResponse.redirect(new URL("/sign-in", request.url));
   }
 
-  return NextResponse.next(); 
+  return NextResponse.next();
 }
