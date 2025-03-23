249b81bb510a6e89036a95d072466ed18c98552c
Sneha Sharma <snehav2109@gmail.com>
2025-03-23 15:01:46 +0530

    update register schema

--- src/schemas/registerSchema.js
+++ src/schemas/registerSchema.js
@@ -6,12 +6,17 @@ export const usernameValidation = z
   .max(20, "Username must be atmost 20 characters")
   .regex(/^[a-zA-Z0-9_]+$/, "Username must not contain special characters");
 
-
 export const registerSchema = z
   .object({
     username: usernameValidation,
     organizationName: z.string().min(1, "Organisation name is required"),
-    address: z.string().min(1, "Address is required"),
+    address: z.object({
+      localAddress: z.string().min(1, "Local Address is required").trim(),
+      city: z.string().min(1, "City is required").trim(),
+      state: z.string().min(1, "State is required").trim(),
+      country: z.string().min(1, "Country is required").trim(),
+      pincode: z.string().min(1, "Pincode is required"),
+    }),
     phoneNumber: z.string().min(10, "Phone number must be 10 digits"),
     email: z.string().email("Invalid email address"),
     password: z.string().min(6, "Password must be at least 6 characters"),
