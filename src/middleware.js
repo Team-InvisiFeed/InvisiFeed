7a63644db0713bb1bd00ead5ef1c8998b8f4d0a6
Sneha Sharma <snehav2109@gmail.com>
2025-03-28 22:45:47 +0530

    update middleware file with Access and refresh token

--- src/middleware.js
+++ src/middleware.js
@@ -9,6 +9,26 @@ export const config = {
 export async function middleware(request) {
   const token = await getToken({ req: request });
   const url = request.nextUrl;
+  console.log("Middleware triggered for:", request.nextUrl.pathname);
+
+  // Check if tokens are expired
+  if (token) {
+    console.log("refreshToken: ", token.refreshToken);
+    if (Date.now() > token.accessTokenExpiry) {
+      const now = Date.now();
+      // If refresh token is expired, redirect to login
+      if (now > token.refreshTokenExpiry) {
+        const response = NextResponse.redirect(
+          new URL("/sign-in", request.url)
+        );
+        // Clear all cookies
+        response.cookies.delete("next-auth.session-token");
+        response.cookies.delete("next-auth.csrf-token");
+        response.cookies.delete("next-auth.callback-url");
+        return response;
+      }
+    }
+  }
 
   if (
     token &&
@@ -21,8 +41,13 @@ export async function middleware(request) {
     );
   }
 
-  if (!token && url.pathname.startsWith("/user")) {
-    return NextResponse.redirect(new URL("/sign-in", request.url));
+  if (token) {
+    if (
+      Date.now() > token.refreshTokenExpiry &&
+      url.pathname.startsWith("/user")
+    ) {
+      return NextResponse.redirect(new URL("/sign-in", request.url));
+    }
   }
 
   return NextResponse.next();
