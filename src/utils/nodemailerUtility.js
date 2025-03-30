3352dc745aaf6e2752630813920639760a77917f
Shubh Verma <shubhverma2003@gmail.com>
2025-03-30 19:33:46 +0530

    update nodemailerUtility to handle attachments

---
+++ src/utils/nodemailerUtility.js
@@ -0,0 +1,30 @@
+import nodemailer from 'nodemailer';
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./env",
+});
+
+const sendEmail = async (email, subject, message, attachment = null) => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    auth: {
+      user: process.env.SMTP_EMAIL, // Your email
+      pass: process.env.SMTP_PASSWORD, // App password
+    },
+  });
+
+  const mailOptions = {
+    from: `"InvisiFeed" <${process.env.SMTP_EMAIL}>`,
+    to: email,
+    subject: subject,
+    text: message, // Plain text version
+    html: message, // HTML version
+    attachments: attachment ? [attachment] : [], // Add attachment if provided
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+export default sendEmail;
