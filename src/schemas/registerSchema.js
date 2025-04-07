b8c57aaa229679de0f1c818588fc40dfcbee1345
Shubh Verma <shubhverma2003@gmail.com>
2025-04-07 16:23:42 +0530

    remove address validation

--- src/schemas/registerSchema.js
+++ src/schemas/registerSchema.js
@@ -10,14 +10,6 @@ export const registerSchema = z
   .object({
     username: usernameValidation,
     organizationName: z.string().min(1, "Organisation name is required"),
-    address: z.object({
-      localAddress: z.string().min(1, "Local Address is required").trim(),
-      city: z.string().min(1, "City is required").trim(),
-      state: z.string().min(1, "State is required").trim(),
-      country: z.string().min(1, "Country is required").trim(),
-      pincode: z.string().min(1, "Pincode is required"),
-    }),
-    phoneNumber: z.string().min(10, "Phone number must be 10 digits"),
     email: z.string().email("Invalid email address"),
     password: z.string().min(6, "Password must be at least 6 characters"),
     confirmPassword: z
