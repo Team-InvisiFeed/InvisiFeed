9f969ef80d290f49eda77a9987c70c523c36746a
Shubh Verma <shubhverma2003@gmail.com>
2025-04-09 21:34:25 +0530

    remove unnecessary imports

--- src/app/api/update-profile/route.js
+++ src/app/api/update-profile/route.js
@@ -1,6 +1,5 @@
 import { NextResponse } from "next/server";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/options";
+
 import dbConnect from "@/lib/dbConnect";
 import OwnerModel from "@/model/Owner";
 
