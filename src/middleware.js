f5ff3091e6b7b42d96c72687d48a3992cd5b8b2c
Shubh Verma <shubhverma2003@gmail.com>
2025-04-12 21:06:17 +0530

    remove unnecessary import

--- src/middleware.js
+++ src/middleware.js
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 export { default } from "next-auth/middleware";
 
