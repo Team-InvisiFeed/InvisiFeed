af902d6b00448fd14cb6ed9bbdd283de57ac24b7
Shubh Verma <shubhverma2003@gmail.com>
2025-04-13 18:37:23 +0530

    add token expiry check on sign-in page

--- src/app/(auth)/sign-in/page.jsx
+++ src/app/(auth)/sign-in/page.jsx
@@ -16,7 +16,7 @@ import {
 import { Input } from "@/components/ui/input"; // Input component
 import { Button } from "@/components/ui/button"; // Button component
 import { signInSchema } from "@/schemas/signinSchema";
-import { signIn } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import { Loader2, Eye, EyeOff } from "lucide-react";
 import { motion } from "framer-motion";
 import { toast } from "sonner";
@@ -27,24 +27,24 @@ function SignInContent() {
   const [isNavigatingToRegister, setIsNavigatingToRegister] = useState(false);
   const searchParams = useSearchParams();
 
-  // const { data: session, status } = useSession();
+  const { data: session, status } = useSession();
   const router = useRouter();
 
   // Check for token expiration
-  // useEffect(() => {
-  //   if (session?.accessToken) {
-  //     const checkTokenExpiry = () => {
-  //       const now = Date.now();
-  //       if (now > session.refreshTokenExpiry) {
-  //         signOut({ redirect: true, callbackUrl: "/sign-in" });
-  //       }
-  //     };
+  useEffect(() => {
+    if (session?.accessToken) {
+      const checkTokenExpiry = () => {
+        const now = Date.now();
+        if (now > session.refreshTokenExpiry) {
+          signOut({ redirect: true, callbackUrl: "/sign-in" });
+        }
+      };
 
-  //     // Check every second
-  //     const interval = setInterval(checkTokenExpiry, 1000);
-  //     return () => clearInterval(interval);
-  //   }
-  // }, [session]);
+      // Check every second
+      const interval = setInterval(checkTokenExpiry, 1000);
+      return () => clearInterval(interval);
+    }
+  }, [session]);
 
   // Setting up React Hook Form with Zod validation schema
   const form = useForm({
