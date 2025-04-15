7a5a03c42dffb25216fabe854e18783b329db113
Shubh Verma <shubhverma2003@gmail.com>
2025-04-15 07:39:35 +0530

    install puppeteer package

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,19 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // ✅ This disables ESLint during `next build`
   },
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        child_process: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
