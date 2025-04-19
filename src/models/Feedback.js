8cbc4823093eaa0cf31f0885445b3dfb39861479
Sneha Sharma <snehav2109@gmail.com>
2025-04-19 13:37:19 +0530

    add feedback model

---
+++ src/models/Feedback.js
@@ -0,0 +1,51 @@
+import mongoose, { Schema } from "mongoose";
+
+const FeedbackSchema = new Schema({
+  givenTo: {
+    type: Schema.Types.ObjectId,
+    ref: "Owner",
+    required: true,
+  },
+  satisfactionRating: {
+    type: Number,
+    required: true,
+  },
+  communicationRating: {
+    type: Number,
+    required: true,
+  },
+  qualityOfServiceRating: {
+    type: Number,
+    required: true,
+  },
+  valueForMoneyRating: {
+    type: Number,
+    required: true,
+  },
+  recommendRating: {
+    type: Number,
+    required: true,
+  },
+  overAllRating: {
+    type: Number,
+    required: true,
+  },
+  feedbackContent: {
+    type: String,
+    trim: true,
+  },
+  suggestionContent: {
+    type: String,
+    trim: true,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const FeedbackModel =
+  mongoose.models.Feedback || mongoose.model("Feedback", FeedbackSchema);
+
+export default FeedbackModel;
