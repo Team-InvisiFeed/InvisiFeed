15c684db9932eb35ad621f14506eaa1be1908788
Shubh Verma <shubhverma2003@gmail.com>
2025-04-25 13:18:43 +0530

    update submit-feedback API for non-anonymous feedbacks

--- src/app/api/submit-feedback/route.js
+++ src/app/api/submit-feedback/route.js
@@ -9,6 +9,8 @@ export async function POST(req) {
 
   const { formData, username, invoiceNumber } = await req.json();
 
+  console.log(formData);
+
   try {
     const owner = await OwnerModel.findOne({ username: username });
 
@@ -32,19 +34,55 @@ export async function POST(req) {
       );
     }
 
+    const {
+      satisfactionRating,
+      communicationRating,
+      qualityOfServiceRating,
+      valueForMoneyRating,
+      recommendRating,
+      overAllRating,
+      feedbackContent,
+      suggestionContent,
+      anonymousFeedback,
+    } = formData;
+
+    console.log(anonymousFeedback);
+
+    let feedback = null;
     // Add the new feedback to the owner's feedbacks array
-
-    const feedback = await FeedbackModel.create({
-      ...formData,
-      givenTo: owner._id,
-    });
-
-    await feedback.save();
+    if (anonymousFeedback) {
+      feedback = await FeedbackModel.create({
+        satisfactionRating,
+        communicationRating,
+        qualityOfServiceRating,
+        valueForMoneyRating,
+        recommendRating,
+        overAllRating,
+        feedbackContent,
+        suggestionContent,
+        givenTo: owner._id,
+      });
+      await feedback.save();
+    } else {
+      feedback = await FeedbackModel.create({
+        satisfactionRating,
+        communicationRating,
+        qualityOfServiceRating,
+        valueForMoneyRating,
+        recommendRating,
+        overAllRating,
+        feedbackContent,
+        suggestionContent,
+        givenTo: owner._id,
+        invoiceId: invoice._id,
+      });
+      await feedback.save();
+    }
 
     // Set the feedback submitted flag on the specific invoice
     invoice.isFeedbackSubmitted = true;
     invoice.feedbackSubmittedAt = new Date();
-    await invoice.save(); 
+    await invoice.save();
 
     owner.feedbacks.push(feedback._id);
     await owner.save();
