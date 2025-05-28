6ff6462731d268500b90c7b4945a1cf932e405c7
Shubh Verma <shubhverma2003@gmail.com>
2025-05-28 16:23:03 +0530

    move Razorpay script to separate Head component

---
+++ src/app/head.js
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <script
+        src="https://checkout.razorpay.com/v1/checkout.js"
+        async
+      ></script>
+    </>
+  );
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -26,12 +26,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <script
-          src="https://checkout.razorpay.com/v1/checkout.js"
-          async
-        ></script>
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
