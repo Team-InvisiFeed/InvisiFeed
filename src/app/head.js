62997f5da6c20ab7c94a59cabce150b82363b645
Shubh Verma <shubhverma2003@gmail.com>
2025-05-28 16:27:03 +0530

    add google-site-verification meta tag to Head component

--- src/app/head.js
+++ src/app/head.js
@@ -1,6 +1,10 @@
 export default function Head() {
   return (
     <>
+      <meta
+        name="google-site-verification"
+        content="zv5irB-R4RF0BMewa4wyOX7BpNnRglGG4x8IP3JHJP8"
+      />
       <script
         src="https://checkout.razorpay.com/v1/checkout.js"
         async
